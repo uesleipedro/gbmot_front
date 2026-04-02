a181d343a8d0712645ff1289b451ec2ff7c9a3e0
uesleipedro <ielseu@hotmail.com>
2026-04-02 15:50:47 -0300

    .

--- src/app/sistema/fichaChamada/page.jsx
+++ src/app/sistema/fichaChamada/page.jsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import moment from "moment";
 moment.locale("pt-br");
 
 export default function FichaChamada() {
-  const alunos = JSON.parse(sessionStorage.getItem("alunos"));
+  const [alunos, setAlunos] = useState([]);
+
+  useEffect(() => {
+    const storedData = sessionStorage.getItem("alunos");
+    setAlunos(JSON.parse(storedData));
+  }, []);
 
   const handlePrint = () => {
     setTimeout(() => window.print(), 100);
